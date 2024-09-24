b633d3f66045683aa67b1d19b392f7a63ef3b87a
plegulluche <plegulluche@gmail.com>
2024-09-24 17:43:15 +0200

    [INIT] init commit, adding libs for discord , envs , creating base bot and creating first command

---
+++ src/commands/ping.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+
+const data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+async function execute(interaction) {
+    await interaction.reply("Pong!");
+}
+
+module.exports = {
+    execute,
+    data,
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages
+  ],
+});
+
+client.on(Events.ClientReady, () => {
+  console.log("Bot is ready");
+});
+
+client.login(process.env.BOT_TOKEN);
